77df5947901ff88abc6ddec7070e0a1436da7dfb
Ricardo Gomes <ricardogoncalvesgomes@gmail.com>
2023-01-17 04:17:10 +0000

    start game in class Game and exit close in JFrame

--- src/com/main/Window.java
+++ src/com/main/Window.java
@@ -8,14 +8,12 @@ public class Window {
 		
 		JFrame frame = new JFrame(title);
 		
-		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
 		frame.setResizable(false);
 		frame.add(game);
 		frame.pack();
 		frame.setLocationRelativeTo(null);
 		frame.setVisible(true);
-		
-		game.start();
 	}
 	
 	public static void main(String[] args) {
